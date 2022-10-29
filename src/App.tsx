a867f7dd4145dcedbe2526f2a8ce97920bdf4aa9
Stefan Temme <temme@objectcode.de>
2022-10-30 00:20:39 +0200

    minor change

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import {allUsers, User} from "./data/User";
 import {getTasksOfTheWeek} from "./tools/TaskManager";
 import {Weekday} from "./tools/Definitions";
+import {TaskType} from "./data/Definitions";
 
 function App() {
     const today = new Date();
@@ -14,14 +15,14 @@ function App() {
                 <h2>{today.toLocaleDateString()}</h2>
             </header>
             {dates.map((d, i) => (
-                <section>
+                <section key={`week_${i}`}>
                     <h4>Woche {i}: {new Date(new Date(d).setDate(d.getDate()-((6+d.getDay())%7))).toLocaleDateString()} - {new Date(new Date(d).setDate(d.getDate()-((6+d.getDay())%7)+6)).toLocaleDateString()}</h4>
                     {allUsers.map(user => (
                         <article key={user}>
                             <h3>{user}</h3>
                             {getTasksOfTheWeek(user as User, d).map((t, i) => (
                                 <div key={`task-${i}`}>
-                                    {Weekday[t.day]} - {t.label}
+                                    {Weekday[t.day]} - {TaskType[t.type]} - {t.label}
                                 </div>
                             ))}
                         </article>
