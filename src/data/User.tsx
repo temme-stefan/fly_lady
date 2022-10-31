6287dc5f1acbdf3dc01f0dd4b0d040402bd26a41
Stefan Temme <temme@objectcode.de>
2022-11-01 00:20:18 +0100

    move icons

--- src/data/User.tsx
+++ src/data/User.tsx
@@ -1,12 +1,15 @@
+import boy from "../img/boy.png"
+import girl from "../img/girl.png"
 
 export enum User {
     Sysy,
     Nappo
 }
+console.log(boy,typeof boy);
 
 export const UserData = new Map([
-    [User.Nappo, {displayName: "Nappo", picture: "img/boy.png"}],
-    [User.Sysy, {displayName: "Sysy", picture: "img/girl.png"}]
+    [User.Nappo, {displayName: "Nappo", picture: boy}],
+    [User.Sysy, {displayName: "Sysy", picture: girl}]
 ])
 
 export const userCount = UserData.size;
