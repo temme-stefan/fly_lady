4f05d1b0f7c6398b6af6de9e1898d67384845cb6
Stefan Temme <temme@objectcode.de>
2022-11-17 12:51:03 +0100

    DateToWeek minorFix

--- src/tools/DateToWeek.tsx
+++ src/tools/DateToWeek.tsx
@@ -1,8 +1,16 @@
 const divisor = 1000 * 60 * 60 * 24 * 7; //ms * sec * min * h * day
-const offset = 4 * 24 * 60 * 60 * 1000; //compare to Monday Jan 05 1970 00:00:00
+const offset = 3 * 24 * 60 * 60 * 1000; //compare to Monday Jan 05 1970 00:00:00
+
+export function toLocalIsoString(date: Date) {
+    const m = date.getMonth() + 1;
+    const month = (m < 10 ? "0" : "") + m;
+    const d = date.getDate();
+    const day = (d < 10 ? "0" : "") + d;
+    return `${date.getFullYear()}-${month}-${day}`;
+}
 
 export function dateToWeek(date: Date): number {
-    const comp = new Date(new Date(date).setHours(0, 0, 0));
+    const comp = new Date(toLocalIsoString(date));
     return Math.floor((comp.getTime() + offset) / divisor);
 }
 
